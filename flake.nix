a1da68c5aee158dd5aceed79fc6e7c391007d29e
W1ngD1nGa5ter <wangyiben1@outlook.com>
2026-06-10 10:00:33 +0800

    refactor(flake): use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,84 +1,90 @@
 {
-  description = "W1ngD1nGa5ter's NixVim configuration";
+  description = "A Nixvim configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      # inputs.nixpkgs.follows = "nixpkgs";
-
-      # Why not `follows`?
-      #
-      # <https://nix-community.github.io/nixvim/user-guide/install.html#accessing-nixvim>
-      # We recommend against using `inputs.nixpkgs.follows = "nixpkgs";` on the `nixvim` input as we test Nixvim against our Nixpkgs revision.
-      # When you use `follows` you opt out of guarantees provided by these tests.
-      # If you choose to use it anyway, removing `follows` should be one of the first debugging steps when encountering issues.
-    };
-
-    flake-utils.url = "github:numtide/flake-utils";
-
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      nixvim,
-      flake-utils,
-      pre-commit-hooks,
-      ...
-    }:
-    {
-      nixvimModules.default = import ./config;
-    }
-    // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        nixvimLib = nixvim.lib.${system};
-        nixvim' = nixvim.legacyPackages.${system};
+    { self, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
-        nixvimModule = {
-          inherit system;
-          module = import ./config;
-          extraSpecialArgs = {
-            inherit self;
-            # inherit (inputs) foo;
+      imports = [
+        # Import nixvim's flake-parts module;
+        # Adds `flake.nixvimModules` and `perSystem.nixvimConfigurations`
+        inputs.nixvim.flakeModules.default
+        # Import git-hooks-nix's flake-parts module for pre-commit hooks
+        inputs.git-hooks-nix.flakeModule
+      ];
+
+      nixvim = {
+        # Automatically install corresponding packages for each nixvimConfiguration
+        # Lets you run `nix run .#<name>`, or simply `nix run` if you have a default
+        packages.enable = true;
+        # Automatically install checks for each nixvimConfiguration
+        # Run `nix flake check` to verify that your config is not broken
+        checks.enable = true;
+      };
+
+      # You can define your reusable Nixvim modules here
+      flake.nixvimModules = {
+        default = ./config;
+      };
+
+      perSystem =
+        {
+          config,
+          system,
+          pkgs,
+          ...
+        }:
+        {
+          # Define the formatter for `nix fmt`
+          formatter = pkgs.nixfmt-tree;
+
+          # Configure pre-commit hooks
+          pre-commit = {
+            settings = {
+              hooks = {
+                nixfmt.enable = true;
+                deadnix.enable = true;
+                statix.enable = true;
+              };
+            };
           };
-        };
-        nvim = nixvim'.makeNixvimWithModule nixvimModule;
-      in
-      {
-        formatter = pkgs.nixfmt-tree;
 
-        checks = {
-          default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-          pre-commit-check = pre-commit-hooks.lib.${system}.run {
-            src = ./.;
-            hooks = {
-              nixfmt.enable = true;
-              deadnix.enable = true;
-              statix.enable = true;
+          # Development shell with pre-commit hooks enabled
+          devShells.default = pkgs.mkShell {
+            # Automatically installs git hooks when entering the shell
+            inherit (config.pre-commit) shellHook;
+          };
+
+          # You can define actual Nixvim configurations here
+          nixvimConfigurations = {
+            default = inputs.nixvim.lib.evalNixvim {
+              inherit system;
+              modules = [
+                self.nixvimModules.default
+              ];
+              # Pass extra arguments to your configuration
+              extraSpecialArgs = {
+                inherit self;
+                # inherit (inputs) foo;
+              };
             };
           };
         };
-
-        packages = {
-          default = nvim;
-        };
-
-        devShells = {
-          default =
-            with pkgs;
-            mkShell {
-              inherit (self.checks.${system}.pre-commit-check) shellHook;
-            };
-        };
-      }
-    );
+    };
 }
